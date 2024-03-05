48b5b4d2df088786cdfc83e2c61a2904cb247f2e
kaimpk <160760354+kaimpk@users.noreply.github.com>
2024-03-05 19:27:27 +0000

    Add checkGameOver function Add multiple rounds

--- javascript.js
+++ javascript.js
@@ -1,31 +1,49 @@
-function getComputerChoice() { 
-    let x = Math.floor(Math.random() *3)
-    return x;
-}
-function playRound(playerSelection, computerSelection)
-{
+let playerScore = 0;
+let computerScore  = 0;
+let isGameOver = false;
     
-    playerSelection === 2 && computerSelection === 1 ?  console.log('You win! rock beats scissors!') :
-    playerSelection === 1 && computerSelection === 0 ?  console.log('You win! scissors beats paper!') :    
-    playerSelection === 0 && computerSelection === 2 ? console.log('You win! rock beats paper!'):       
-    playerSelection === 1 && computerSelection === 2 ? console.log('You lose! scissors beats rock!') :
-    playerSelection === 0 && computerSelection === 1 ? console.log('You lose! paper beats scissors!') :
-    playerSelection === 2 && computerSelection === 0 ? console.log("You lose! paper beats rock!") :
-    console.log("Draw!");
-        
     
-}
-const playerInput="rock";
-let playerSelection;
-switch(playerInput){
-    case("rock") : playerSelection =2
-    break;
-    case("scissors"): playerSelection =1
-    break;
-    case("paper"): playerSelection =0
-    break;
-    default: console.log("nothing selected")
-    break;
-}
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection))
+   do {
+    function checkGameOver(computerScore, playerScore){
+        if(playerScore ===5){
+            isGameOver= true;}
+            else if(computerScore === 5)
+            {
+                isGameOver = true;
+         }
+         else{}
+    }
+     function getComputerChoice() { 
+         let x = Math.floor(Math.random() *3)
+         return x;
+     }
+     function playRound(playerSelection, computerSelection)
+     {
+         
+          if(playerSelection === "rock" && computerSelection === 1){  console.log('You win! rock beats scissors!')
+     playerScore++;} 
+         else if(playerSelection === "scissors" && computerSelection === 0){  console.log('You win! scissors beats paper!')
+          playerScore++; }    
+         else if(playerSelection === "paper" && computerSelection === 2){ console.log('You win! rock beats paper!')
+         playerScore++;}     
+         else if(playerSelection === "scissors" && computerSelection === 2){ console.log('You lose! scissors beats rock!') 
+     computerScore++;}
+         else if(playerSelection === "paper" && computerSelection === 1){ console.log('You lose! paper beats scissors!') 
+         computerScore++;}
+         else if(playerSelection === "rock" && computerSelection === 0){ console.log("You lose! paper beats rock!") 
+         computerScore++;}
+         else {console.log("Draw!");}
+             
+         
+     } 
+     const computerSelection = getComputerChoice();
+     const playerInput = prompt("Rock, Paper or Scissors?");
+     let playerSelection = playerInput.toLowerCase();
+     
+     console.log(playRound(playerSelection, computerSelection))
+     console.log(`Player score: ${playerScore}. Computer score: ${computerScore}`)
+     checkGameOver(computerScore, playerScore)
+     
+   } while (isGameOver === false);
+
+playerScore === 5 ? console.log("player wins!") : console.log("computer wins!")
